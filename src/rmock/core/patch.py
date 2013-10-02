0e8f5c805d8e888775cda014dc13bcc9aabff12f
Krzysztof Chmiel <tikan@misery>
2013-10-02 14:56:17 +0200

    Add documentation for Patch decorator

--- src/rmock/core/patch.py
+++ src/rmock/core/patch.py
@@ -23,8 +23,36 @@ from inspect import isclass
 from rmock.config import get_config
 
 class Patch(object):
-    
+
+    """
+    Decorator or context manager which injects properly configured mock into the current context.
+
+    If Patch is used as a method decorator than it a mock is created before each method call,
+    injected as a first parameter and than finalized after the method returns.
+    If it's used as a class decorator than a mock is created on class definition and it's
+    reused in all test method calls, just being resetted before each call. Be default
+    the mock objects is passed as a first argument to each method.
+    If you want the mock to be stored in class-level attribute instead than pass the attribute name
+    as a `classvar` attribute to `Patch` __init__ method.
+    This class could also be used as a context manager - in this case mock object is created and returned
+    from __enter__ method and finalized in __exit__ method.
+
+    In that moment `Patch` decorator doesn't work for free (non-method) functions, it should be
+    considered as a bug and will be fix in the future. You could use context manager
+    around whole function body instead for now.
+
+    Additionally, because of the way in which class decorators work in python subclasses of a class decorated
+    with `Patch` wouldn't have its test methods wrapped with reset_mock calls.
+    The simple workaround for this problem is to manually call reset_mock in setup method in the base class.
+    Note that in future that behaviour may be changed.
+    """
+
     def __init__(self, run_manager, *mock_args, **mock_kwargs):
+        """
+        Initialize Patch object with given attributes
+
+        The `args` and `kwargs` and generally the same
+        """
         self.run_manager = run_manager
         self.mock_args = mock_args
         self.mock_kwargs = mock_kwargs
@@ -33,7 +61,6 @@ class Patch(object):
         self.classvar = mock_kwargs.pop('classvar', None)
     
     def __call__(self, test_object):
-                
         if isclass(test_object):
             test_class = test_object
             mock = self.run_manager.run(*self.mock_args, **self.mock_kwargs)
@@ -65,7 +92,6 @@ class Patch(object):
             return wrapped
     
     def _make_function_wrapper(self, test_function, mock):
-        
         @functools.wraps(test_function)
         def wrapped(function_self, *args, **kwargs):
             
@@ -84,7 +110,6 @@ class Patch(object):
         return wrapped
     
     def _list_test_functions(self, test_class):
-        
         test_method_prefix = get_config()['test_method_prefix']
         
         for attr in dir(test_class):
